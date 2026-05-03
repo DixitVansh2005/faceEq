6a4fbbf426b0ce56f0802d0f470ee929cba0ea76
DixitVansh2005 <vanshdixit31@gmail.com>
2026-05-03 18:25:18 +0530

    refactoring

--- detector.py
+++ detector.py
@@ -9,7 +9,7 @@ import time
 import streamlit as st
 
 
-ANALYSIS_ACTIONS = ['age', 'gender', 'emotion', 'race']
+ANALYSIS_ACTIONS = ['emotion']
 DETECTOR_BACKEND = 'opencv'  # lightweight, no extra download, fine for most photos
 
 
